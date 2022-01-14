1d0e8d060cea3bc70d5f85f43a367c9944187c24
Anita Samchuk <71382097+Fokkely@users.noreply.github.com>
2022-01-15 02:51:04 +0300

    Trying to make a parser

--- CatApp/TableViewCellViewModel.swift
+++ CatApp/TableViewCellViewModel.swift
@@ -17,3 +17,22 @@ struct TableViewCellViewModel {
         image = UIImage(systemName: "person")
     }
 }
+
+struct Parser {
+    
+    func parse(comp: @escaping ([Cat])->()) {
+        
+        guard let api = URL(string: "https://api.thecatapi.com/v1/breeds") else {return}
+        var request = URLRequest(url: api)
+        request.addValue("a56ce6a7-1d1b-43c7-ade0-4685a3b3729", forHTTPHeaderField: "TRN-Api-Key")
+        URLSession.shared.dataTask(with: api) {
+            (data, response, error) in
+            do {
+                let result = try JSONDecoder().decode(Array.self, from: data ?? Data())
+                comp(result.modules)
+            } catch {
+                print(String(describing: error))
+            }
+        }.resume()
+    }
+}
--- CatApp/ViewController.swift
+++ CatApp/ViewController.swift
@@ -9,7 +9,9 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource {
     
-    private var models = [Cat]()
+    var models = [Cat]()
+    
+    let parser = Parser()
     
     private let tableView: UITableView = {
         let table = UITableView()
@@ -19,31 +21,37 @@ class ViewController: UIViewController, UITableViewDataSource {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureModels()
+        parser.parse {
+            data in
+            self.models = data
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+//        configureModels()
         view.addSubview(tableView)
         tableView.dataSource = self
         tableView.frame = view.bounds
     }
     
-    private func configureModels() {
-        let breeds = [" American Bobtail", " Abyssian", "Devon Rex"]
-        for breed in breeds {
-            models.append(Cat(breed: breed, image: nil))
-        }
-    }
-    
+//    private func configureModels() {
+//        let breeds = [" American Bobtail", " Abyssian", " Devon Rex"]
+//        for breed in breeds {
+//            models.append(Cat(breed: breed, image: nil))
+//        }
+//    }
+//
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return models.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let model = models[indexPath.row]
         guard let cell = tableView.dequeueReusableCell(
             withIdentifier: TableViewCell.identifier,
             for: indexPath
         ) as? TableViewCell else {return UITableViewCell()}
         
-        cell.configure(with: TableViewCellViewModel(with: model))
+        cell.configure(with: TableViewCellViewModel(with: models[indexPath.row]))
         return cell
     }
 
